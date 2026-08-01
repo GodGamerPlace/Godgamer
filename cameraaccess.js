22789f584777e7b0be5900836c21a26266de3fea
GodGamerPlace <amanjakhar211@gmail.com>
2026-08-01 19:13:07 +0530

    Add alert for JavaScript file confirmation
    Added an alert to confirm JavaScript file linkage.

--- cameraaccess.js
+++ cameraaccess.js
@@ -1,3 +1,8 @@
+alert("JavaScript file is linked and working!");
+
+// Your existing camera code goes below this line
+const videoElement = document.querySelector("#camera-stream");
+// ... rest of your code
 const videoElement = document.querySelector("#camera-stream");
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(stream => {
